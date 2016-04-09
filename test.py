0b642a4625bc151b826910bc4de7a37f491d10ce
Pei Li <peihongli94@gmail.com>
2016-04-08 23:39:54 -0400

    change sim

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ f = open("data.dat", 'w')
 sum_balance = 0
 min_balance = 0
 min_running_balance = 0
-for i in range(100):
+for i in range(1000):
 	sim = Simulation(8)
 	res = sim.run()
 	sum_balance += res
@@ -13,5 +13,5 @@ for i in range(100):
 	f.write(str(res) + "\n")
 
 print("Final balance: " + str(sum_balance))
-print("Min running balance: " + str(min_running_balance))
-print("Min balance: " + str(min_balance))
+#print("Min running balance: " + str(min_running_balance))
+#print("Min balance: " + str(min_balance))
